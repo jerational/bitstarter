367c1e43c8becdfc930f1f7ab0288a28e03a0833
Ubuntu <ubuntu@ip-172-31-28-180.us-west-2.compute.internal>
2014-03-27 22:21:21 +0000

    changed Web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var bufferReading;
 app.use(express.logger());
 
+fs.readFile('index.html', function(err, data) {
+    var buffer = new Buffer(data);
+
+    bufferReading = buffer.toString();
+}
 app.get('/', function(request, response) {
-  response.send('Hello Lucas is Gay!');
+  response.send(bufferReading);
 });
 
 var port = process.env.PORT || 5000;
